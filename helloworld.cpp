2b36a8522850a8b4b6d0ead019410d90befec49c
莫迪茵 <Mdy20221001@outlook.com>
2025-12-05 13:49:42 +0800

    modify1

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,8 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int complus(int a, int b)
+{
+  return a + b;
+}
+
 int main()
 {
-  cout<<"hello,world!"<<endl;
+  cout << "hello,world!" << endl;
+  int a, b;
+  cin >> a >> b;
+  cout << complus(a, b) << endl;
   return 0;
 }
